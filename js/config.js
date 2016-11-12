fa4a8bdf9f5a9e5302f9f7e4de1eb2eaa3f89f7d
leandrofra <leandro.franzoni@serasoft.it>
2016-11-13 00:04:40 +0100

    Config changed

--- js/config.js
+++ js/config.js
@@ -3,20 +3,20 @@ $(function() {
   CMS.init({
 
     // Name of your site or location of logo file, relative to root directory (img/logo.png)
-    siteName: 'My Site',
+    siteName: 'CMS',
 
     // Tagline for your site
-    siteTagline: 'Your site tagline',
+    siteTagline: 'Prova',
 
     // Email address
-    siteEmail: 'your_email@example.com',
+    siteEmail: 'liosc@example.com',
 
     // Name
-    siteAuthor: 'Your Name',
+    siteAuthor: 'Me',
 
     // Navigation items
     siteNavItems: [
-      { name: 'Github', href: 'https://github.com/yourname', newWindow: false},
+      { name: 'Github', href: 'https://github.com/liosc', newWindow: false},
       { name: 'About'}
     ],
 
@@ -53,8 +53,8 @@ $(function() {
 
      // If Github mode is set, your Github username and repo name.
     githubUserSettings: {
-      username: 'yourusername',
-      repo: 'yourrepo'
+      username: 'liosc',
+      repo: 'cms'
     },
 
     // If Github mode is set, choose which Github branch to get files from.
